3e84c1102824b2a79cccbbb1842d2a4210044771
Andrei Teodorescu <andrei.teodorescu@dcyphermedia.com>
2021-02-11 10:58:44 +0200

    End of section 5

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import {map} from 'rxjs/operators';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+//services are singleton
+//data gets destroys only when application is closed down
+//services are usually used for http requests
+
+export class AccountService {
+
+  baseUrl = 'https://localhost:5001/api/';
+
+  //buffer object, it will emit the last values inside it, it will store only one User (null/current user)
+  private currentUserSource = new ReplaySubject<User>(1);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  login(model : any){
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if(user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  register(model: any){
+    return this.http.post(this.baseUrl + 'account/register', model).pipe(
+      map((user : User) => {
+        if(user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+        /* return user; */
+      })
+    )
+  }
+
+  setCurrentUser(user: User){
+    this.currentUserSource.next(user);
+  }
+
+  logout(){
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+}
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { error } from 'protractor';
+import { AccountService } from '../_services/account.service';
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  /* @Input() usersFromHomeComponent: any; */
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    this.accountService.register(this.model).subscribe(response => {
+      console.log(response);
+      this.cancel();
+    }, error => {
+      console.log(error);
+    })
+  };
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
